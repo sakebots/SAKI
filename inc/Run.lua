a865ed9a5fe808b0c8f9cb834278ef96982a4539
sakebots <85326505+sakebots@users.noreply.github.com>
2021-06-04 20:49:15 +0300

    Add files via upload

--- inc/Run.lua
+++ inc/Run.lua
@@ -452,7 +452,7 @@ if amrr[1] == Amor then
 amrnew = Amor ; amrold = ik   
 end end end
 Text = Text:gsub(amrnew,amrold)
-AF = CheckBotA(msg) if AF then return sendMsg(msg.chat_id_,msg.id_,AF) end 
+
 GetMsg = ScriptFile.iSAKE(msg,{Text:match(Text2)})
 if GetMsg then
 print("\27[1;35m¦This_Msg : "..Text2.."  | Plugin is: \27[1;32mScript.lua\27[0m")
@@ -467,7 +467,7 @@ Text = Text:gsub("ی","ي")
 Text = Text:gsub("ک","ك")
 Text = Text:gsub("ه‍","ه")
 if Text:match(SAKE) then -- Check Commands To admin
-AF = CheckBotA(msg) if AF then return sendMsg(msg.chat_id_,msg.id_,AF) end 
+
 GetMsg = ScriptFile.iSAKE(msg,{Text:match(SAKE)})
 if GetMsg then
 print("\27[1;35m¦This_Msg : ",SAKE.." | Plugin is: \27[1;32mScript.lua\27[0m")
